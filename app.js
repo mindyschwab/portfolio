78d5ead31dd1123085f4357294cb0df015044d8e
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-02-20 15:36:30 -0800

    added and styled resume content for modal

---
+++ app.js
@@ -0,0 +1,4 @@
+// **** Resume Modal ******
+const resumeLink = document.getElementById('resume');
+const resumeDoc = document.getElementById('redume-modal')
+const closeBtn = document.getElementById('close-modal')
