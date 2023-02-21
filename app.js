a0dbcf0776a5037a03aac832bfda542de68ac201
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-02-20 18:44:04 -0800

    added a little resume formatting

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // **** Resume Modal ******
-// const resumeLink = document.getElementById('resume');
-// console.log(resumeLink);
-// const resumeDoc = document.getElementById('resume-modal')
-// const closeBtn = document.getElementById('close-modal')
+const resumeLink = document.getElementById('resume');
+console.log(resumeLink);
+const resumeDoc = document.getElementById('resume-modal')
+const closeBtn = document.getElementById('close-modal')
 
-// const openResume = () => {
-//     resumeDoc.style.display = 'block';
-//     // document.body.style.background = ('rgba(0, 0, 0, 0.75)')
-// }
+const openResume = () => {
+    resumeDoc.style.display = 'block';
+    // document.body.style.background = ('rgba(0, 0, 0, 0.75)')
+}
 
-// const closeResume = () => {
-//     resumeDoc.style.display = 'none';
-// }
+const closeResume = () => {
+    resumeDoc.style.display = 'none';
+}
 
-// resumeLink.addEventListener('click', openResume);
-// closeBtn.addEventListener('click', closeResume);
+resumeLink.addEventListener('click', openResume);
+closeBtn.addEventListener('click', closeResume);
 
 
 // ***** Project Image Carousel****
