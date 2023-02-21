aae8c3e1504c63b8a2de7f4fff5c087380d22067
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-02-20 17:41:22 -0800

    adding JS modal- not working yet and project images

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // **** Resume Modal ******
 const resumeLink = document.getElementById('resume');
-const resumeDoc = document.getElementById('redume-modal')
+console.log(resumeLink);
+const resumeDoc = document.getElementById('resume-modal')
 const closeBtn = document.getElementById('close-modal')
+
+const openResume = () => {
+    resumeDoc.style.display = 'block';
+    document.body.style.background = ('rgba(0, 0, 0, 0.75)')
+}
+
+const closeResume = () => {
+    resumeDoc.style.display = 'none';
+}
+
+resumeLink.addEventListener('click', openResume);
+closeBtn.addEventListener('click', closeResume);
