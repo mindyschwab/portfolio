fe2128f2f8b4a233bc881402f3c77ce2fbb9d633
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-02-27 08:59:43 -0800

    removed modal code from app.js

--- app.js
+++ app.js
@@ -13,22 +13,6 @@ function navColor() {
     }
 }
 
-// // **** Resume Modal ******
-// const resumeLink = document.getElementById('resume');
-// const resumeDoc = document.getElementById('resume-modal')
-// const closeBtn = document.getElementById('close-modal')
-
-// const openResume = () => {
-//     resumeDoc.style.display = 'block';
-// }
-
-// const closeResume = () => {
-//     resumeDoc.style.display = 'none';
-// }
-
-// resumeLink.addEventListener('click', openResume);
-// closeBtn.addEventListener('click', closeResume);
-
 // ***** Project Image Carousel****
 let currentImgIndex = 0;
 let previousImgIndex = 0;
